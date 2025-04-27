eadb8ff2092c5aa7c27ceec227cbc8ce233ce049
kavinesh <kavineshkavinesh860@gmail.com>
2025-04-27 12:47:36 +0530

    added gitignoer

--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://xibkrdoizcczwcigmlxfz.supabase.co";
+const supabaseUrl = "https://ibkrdoizcczwcigmlxfz.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlia3Jkb2l6Y2N6d2NpZ21seGZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NTIzMzIsImV4cCI6MjA2MTIyODMzMn0.GLj2l85pgQA4dmPlxULuXro_kAnrtJJaxMlI5mq2Jfs";
 const supabase = createClient(supabaseUrl, supabaseKey);
